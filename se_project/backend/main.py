db9eed58d361599dcba620b4afbc088ac7cc219d
PeachBlackTea <134840128+PeachBlackTea@users.noreply.github.com>
2025-06-16 20:17:58 +0900

    Update main.py

--- se_project/backend/main.py
+++ se_project/backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from models.LLM.app.llm.routes import router as llm_router
-# vlm은 나중에 처리 주석
+# vlm 라우터는 나중에 필요할 때 주석 해제
 # from models.vlm.routes import router as vlm_router
 
 app = FastAPI()
