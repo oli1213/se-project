e2427c1e8899265f2899e83d6606e251d932d342
PeachBlackTea <134840128+PeachBlackTea@users.noreply.github.com>
2025-06-16 22:11:47 +0900

    Update main.py

--- se_project/backend/main.py
+++ se_project/backend/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from backend.routes import router as backend_router
 from models.LLM.app.llm.routes import router as llm_router
-#from models.vlm.routes import router as vlm_router  
+from models.vlm.routes import router as vlm_router  
 
 app = FastAPI()
 
 app.include_router(backend_router, prefix="/backend", tags=["Backend"])
 app.include_router(llm_router, prefix="/llm", tags=["LLM"])
-#app.include_router(vlm_router, prefix="/vlm", tags=["VLM"])
+app.include_router(vlm_router, prefix="/vlm", tags=["VLM"])
 
 @app.get("/")
 async def root():
