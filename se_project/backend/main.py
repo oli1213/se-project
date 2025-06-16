7be8153179eba93abdd597fea94da58420c0a0bf
PeachBlackTea <134840128+PeachBlackTea@users.noreply.github.com>
2025-06-16 11:04:01 +0900

    main.py

--- se_project/backend/main.py
+++ se_project/backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from models.llm.routes import router as llm_router
-from models.vlm.routes import router as vlm_router
+from models.LLM.app.llm.routes import router as llm_router
+# vlm은 나중에 처리 주석
+# from models.vlm.routes import router as vlm_router
 
 app = FastAPI()
 
 app.include_router(llm_router, prefix="/llm", tags=["LLM"])
-app.include_router(vlm_router, prefix="/vlm", tags=["VLM"])
+# app.include_router(vlm_router, prefix="/vlm", tags=["VLM"])
 
 @app.get("/")
 async def root():
