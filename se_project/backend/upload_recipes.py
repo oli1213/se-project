aecd91d599686c262bb0a8ad5100905c66e6ac34
PeachBlackTea <134840128+PeachBlackTea@users.noreply.github.com>
2025-06-16 20:37:11 +0900

    Update upload_recipes.py

--- se_project/backend/upload_recipes.py
+++ se_project/backend/upload_recipes.py
@@ -2,18 +2,14 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import json
 
-# 1. 서비스 계정 키로 초기화
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
-# 2. Firestore 클라이언트 연결
 db = firestore.client()
 
-# 3. 수정된 JSON 파일 열기
 with open("recipes_updated.json", "r", encoding="utf-8") as f:
     recipes = json.load(f)
 
-# 4. Firestore에 데이터 업로드
 for recipe in recipes:
     db.collection("recipes").add(recipe)
 
