7ac79fd2df828d44974a422407dab0d6d6200d62
PeachBlackTea <134840128+PeachBlackTea@users.noreply.github.com>
2025-06-19 17:50:48 +0900

    Update main.py

--- se_project/backend/main.py
+++ se_project/backend/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routes import router as backend_router
 from models.LLM.app.llm.routes import router as llm_router
 from models.vlm.routes import router as vlm_router  
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  
+    "http://127.0.0.1:3000",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(backend_router, prefix="/backend", tags=["Backend"])
 app.include_router(llm_router, prefix="/llm", tags=["LLM"])
